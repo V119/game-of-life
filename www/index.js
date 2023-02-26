eb0dcae8c164cec6e223dff72d654a01fdbddc92
yanghao <928131755@qq.com>
2023-02-26 18:44:00 +0800

    second

--- www/index.js
+++ www/index.js
@@ -37,16 +37,22 @@ const getIdx = (x, y) => {
     return y * width + x;
 }
 
+const bitIsSet = (n, arr) => {
+    let byte = Math.floor(n / 8);
+    let mask = 1 << n % 8;
+    return (arr[byte] & mask) === mask;
+}
+
 const drawCells = () => {
     const cellPrt = universe.cells();
-    const cells = new Uint8Array(memory.buffer, cellPrt, width * height);
+    const cells = new Uint8Array(memory.buffer, cellPrt, width * height / 8);
 
     ctx.beginPath();
 
     for (let x = 0; x < width; x++) {
         for (let y = 0; y < height; y++) {
             const idx = getIdx(x, y);
-            ctx.fillStyle = cells[idx] === Cell.Dead ? DEAD_COLOR : ALIVE_COLOR;
+            ctx.fillStyle = bitIsSet(idx, cells) ? ALIVE_COLOR : DEAD_COLOR;
             ctx.fillRect(
                 x * (CELL_SIZE + 1) + 1,
                 y * (CELL_SIZE + 1) + 1,
@@ -58,15 +64,98 @@ const drawCells = () => {
 
     ctx.stroke();
 }
+
+canvas.addEventListener("click", event => {
+    const boundingRect = canvas.getBoundingClientRect();
+
+    const scaleX = canvas.width / boundingRect.width;
+    const scaleY = canvas.height / boundingRect.height;
+
+    const canvasLeft = (event.clientX - boundingRect.left) * scaleX;
+    const canvasTop = (event.clientY - boundingRect.top) * scaleY;
+
+    const row = Math.min(Math.floor(canvasTop / (CELL_SIZE + 1)), height - 1);
+    const col = Math.min(Math.floor(canvasLeft / (CELL_SIZE + 1)), width - 1);
+
+    universe.toggle_cell(row, col);
+
+    drawGrid();
+    drawCells();
+});
+
+const fps = new class {
+    constructor() {
+        this.fps = document.getElementById("fps");
+        this.frames = [];
+        this.lastFrameTimeStamp = performance.now();
+    }
+
+    render() {
+        const now = performance.now();
+        const delta = now - this.lastFrameTimeStamp;
+        const fps = 1 / delta * 1000;
+
+        this.frames.push(fps);
+        if (this.frames.length > 100) {
+            this.frames.shift();
+        }
+
+        let min = Infinity;
+        let max = -Infinity;
+        let sum = 0;
+        for (let i = 0; i < this.frames.length; i++) {
+            sum += this.frames[i];
+            max = Math.max(max, this.frames[i]);
+            min = Math.min(min, this.frames[i]);
+        }
+
+        let mean = sum / this.frames.length;
+        this.fps.textContent = `
+        Frames Per Second:
+                 latest = ${Math.round(fps)}
+        avg of last 100 = ${Math.round(mean)}
+        min of last 100 = ${Math.round(min)}
+        max of last 100 = ${Math.round(max)}
+        `.trim();
+    }
+};
+
+let animationId = null;
+
 const renderLoop = () => {
+    fps.render();
+
     universe.tick();
 
     drawGrid();
     drawCells();
 
-    requestAnimationFrame(renderLoop);
+    animationId = requestAnimationFrame(renderLoop);
 }
 
-drawGrid();
-drawCells();
-requestAnimationFrame(renderLoop);
+const isPaused = () => {
+    return animationId === null;
+}
+
+const playPauseButton = document.getElementById("play-pause");
+
+const play = () => {
+    playPauseButton.textContent = "⏸";
+    renderLoop();
+}
+
+const pause = () => {
+    playPauseButton.textContent = "▶";
+    cancelAnimationFrame(animationId);
+    animationId = null;
+}
+
+playPauseButton.addEventListener("click", event => {
+    if (isPaused()) {
+        play();
+    } else {
+        pause();
+    }
+});
+
+play();
